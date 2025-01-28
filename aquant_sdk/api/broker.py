27d10d0700776f6a7886dffbf6e82f50fed1cd52
Davi Ferreira <academic.dave.ito@gmail.com>
2025-01-28 19:38:38 +0000

    feature(software_development_kit): adds api requests for the sdk

--- aquant_sdk/api/broker.py
+++ aquant_sdk/api/broker.py
@@ -6,9 +6,6 @@ from aquant_sdk.settings import settings
 
 
 def lru_cache_wrapper(maxsize: int = 1000) -> Callable:
-    """
-    Retorna uma instância de cache LRU encapsulada em uma função independente.
-    """
     return lru_cache(maxsize=maxsize)
 
 
@@ -18,32 +15,13 @@ class BrokerService:
         self._get_brokers_name_cached = lru_cache_wrapper()(self._fetch_broker_name)
 
     async def get_brokers_name(self, broker_id: int) -> str:
-        """
-        Obtém o nome de um broker com base no ID fornecido.
-
-        Args:
-            broker_id (int): ID do broker.
-
-        Returns:
-            str: Nome do broker ou mensagem padrão caso não seja encontrado.
-        """
         return await self._get_brokers_name_cached(broker_id)
 
     async def _fetch_broker_name(self, broker_id: int) -> str:
-        """
-        Método interno para buscar o nome do broker da API.
-
-        Args:
-            broker_id (int): ID do broker.
-
-        Returns:
-            str: Nome do broker ou mensagem padrão caso não seja encontrado.
-        """
         try:
             url = f"{settings.BROKER_API}/{broker_id}"
             response = await self.http_client.get(url)
 
-            # Valida a estrutura da resposta
             if response.status_code == 200:
                 data = response.json()
                 return data.get("name", "Name not found")
